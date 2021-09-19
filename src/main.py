bcb3e9eb20cf069a1b68e93f3e7a7fb3c939f108
Jaime Andres Torres B <69700780+XaurDesu@users.noreply.github.com>
2021-09-19 14:36:09 -0500

    Add Skeleton

---
+++ src/main.py
@@ -0,0 +1,17 @@
+"""
+---IMAGE AUGMENTER---
+
+Simple script for the augmentation of images.
+
+Done by Jaime Andres Torres Bermejo for ROBOCOL.
+Use it if you want to i'm not a cop.
+"""
+
+import heavy_augmenter as ha
+import simple_augmenter as sa
+
+def main():
+    print("Image Augmenter.")
+
+if __name__ == "__main__":
+    main()
